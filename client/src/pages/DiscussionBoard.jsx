162d5fa12db820c286a539d857503993da072371
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-02 01:46:18 -0400

    DiscussionBoard template added

---
+++ client/src/pages/DiscussionBoard.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { getAllPosts } from '../utilities/postService';
+import Post from '../components/Post';
+
+const DiscussionBoard = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const data = await getAllPosts();
+        setPosts(data);
+      } catch (error) {
+        console.error('Failed to fetch posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Discussion Board</h2>
+      {/* {posts.map(post => (
+        <Post key={post.id} post={post} onDelete={} onEdit={} />
+      ))} */}
+    </div>
+  );
+};
+
+export default DiscussionBoard;
