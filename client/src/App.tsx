5524c747035bd76372c78c9077bc30272e9d6e9d
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-11-03 19:05:47 -0400

    Milestone 3 update

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { RouterProvider } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-5xl">
+        Hi This is playpal
+      </div>
+    </>
+  )
+}
+
+export default App
