19f7b51a1a89a83ccf3b17751241f4bbd8a5ba79
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-03 17:49:50 -0400

    updated the home css file location

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGraduationCap, faUsers, faCalendar, faTrophy } from '@fortawesome/free-solid-svg-icons';
-import './Home.css'; // Import the Home.css file
+import '../css/Home.css';
 
 const Home = () => {
   return (
