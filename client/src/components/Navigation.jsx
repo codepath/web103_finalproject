eb0d196f2465f7525b396cfc801d267ad5984694
Myesha <maisha.mahjabin27@gmail.com>
2023-11-03 16:25:45 -0400

    Created Home Page

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
   return (
     <nav>
-      <NavLink to="/" end>Home</NavLink>
-      <NavLink to="/posts">Discussion Board</NavLink>
-      <NavLink to="/resources">Resources</NavLink>
+      {/* <NavLink to="/" end>Home</NavLink> */}
+      {/* <NavLink to="/posts">Discussion Board</NavLink>
+      <NavLink to="/resources">Resources</NavLink> */}
       {/* Add other navigation links as needed */}
     </nav>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,62 @@
-// Home.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap, faUsers, faCalendar, faTrophy } from '@fortawesome/free-solid-svg-icons';
+import './Home.css'; // Import the Home.css file
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to CodeFM</h1>
-      <nav>
-        <Link to="/posts">Discussion Board</Link>
-        <Link to="/resources">Learning Resources</Link>
-      </nav>
+      <div className="navbar">
+        <img src="/logo.PNG" alt="Logo" className="logo" />
+        <div className="link-container">
+          <Link to="/about" className="link">
+            About Us
+          </Link>
+          <Link to="/events" className="link">
+            Events
+          </Link>
+          <Link to="/resources" className="link">
+            Resources
+          </Link>
+        </div>
+        <nav>
+          <Link to="/login" className="link join-us">
+            Login
+          </Link>
+        </nav>
+      </div>
+      <div className="image-container">
+        <img src="/home.jpeg" alt="Image" className="image" />
+        <Link to="/join" className="join-us-button">Join Us</Link>
+      </div>
+      <div className="resource-bar">
+        <div className="text-container">
+          <FontAwesomeIcon icon={faGraduationCap} className="icon" />
+          <span>Resource</span>
+        </div>
+        <div className="text-container">
+          <FontAwesomeIcon icon={faUsers} className="icon" />
+          <span>Community</span>
+        </div>
+        <div className="text-container">
+          <FontAwesomeIcon icon={faCalendar} className="icon" />
+          <span>Events</span>
+        </div>
+        <div className="text-container">
+          <FontAwesomeIcon icon={faTrophy} className="icon" />
+          <span>Skills</span>
+        </div>
+      </div>
+      <div className="bar2">
+        <span className="bar2-text">Unlock Your Learning Potential With CodeFM!</span>
+        <Link to="/join" className="join-us-button-mobile">Join Us</Link>
+      </div>
+      <div className="footer">
+        &copy; {new Date().getFullYear()} CodeFM. All rights reserved.
+      </div>
     </div>
   );
 };
 
 export default Home;
-
