02fefefaee34cb8a624b1d0f1f7dcc02c24374eb
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-04 20:28:41 -0400

    post changes

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -8,7 +8,7 @@ export const getAllPosts = async () => {
     const response = await axios.get(API_URL);
     return response.data;
   } catch (error) {
-    console.error('Error fetching posts:', error);
+    console.error('Error fetching posts:', error); //get
     throw error;
   }
 };
@@ -18,7 +18,7 @@ export const createPost = async (postData) => {
     const response = await axios.post(API_URL, postData);
     return response.data;
   } catch (error) {
-    console.error('Error creating post:', error);
+    console.error('Error creating post:', error); //create
     throw error;
   }
 };
--- server/routes/PostRoutes.js
+++ server/routes/PostRoutes.js
@@ -3,8 +3,8 @@ import { getAllPosts, createPost } from '../controllers/PostController.js';
 
 const router = express.Router();
 
-router.get('/', getAllPosts);
-router.post('/', createPost);
+router.get('/', getAllPosts); //read
+router.post('/', createPost); //create
 // Uncomment and implement these in PostController.js if needed
 // router.patch('/:id', updatePost);
 // router.delete('/:id', deletePost);
