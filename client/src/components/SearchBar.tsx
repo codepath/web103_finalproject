85b322b018975b70ffcb31c882413c50136242aa
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-11-04 20:19:20 -0400

    Frontend Update

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,37 +1,37 @@
-import React from "react";
-import { useRef } from "react";
-import { BsSearch } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
-import useGameQueryStore from "../store";
+import React from 'react'
+import { useRef } from 'react'
+import { BsSearch } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+import useGameQueryStore from '../store'
 
 const SearchBar = () => {
-  const ref = useRef<HTMLInputElement>(null);
-  const setSearchText = useGameQueryStore(s => s.setSearchText);
-  const navigate = useNavigate();
+    const ref = useRef<HTMLInputElement>(null)
+    const setSearchText = useGameQueryStore((s) => s.setSearchText)
+    const navigate = useNavigate()
 
-  return (
-    <form 
-    className="w-full"
-    onSubmit={(event) => {
-      event.preventDefault();
-      if (ref.current) {
-        setSearchText(ref.current.value);
-        navigate('/');
-      }
-    }}>
-      <div className="flex flex-row justify-center w-full gap-2 p-4">
-            
-            <input
-                type="text"
-                className="w-full h-[30px] rounded-lg"
-                placeholder="Search for games..."
-            />
-            <div className="fill-white flex flex-col justify-center align-bottom">
-                <BsSearch className="fill-white "/>
+    return (
+        <form
+            className="w-full"
+            onSubmit={(event) => {
+                event.preventDefault()
+                if (ref.current) {
+                    setSearchText(ref.current.value)
+                    navigate('/')
+                }
+            }}
+        >
+            <div className="flex flex-row justify-center w-full gap-4 p-4">
+                <input
+                    type="text"
+                    className="w-full h-[30px] rounded-xl"
+                    placeholder="  Search for games..."
+                />
+                <div className="fill-white flex flex-col justify-center align-bottom">
+                    <BsSearch className="fill-white " size={25} />
+                </div>
             </div>
-        </div>
-    </form>
-  );
-};
+        </form>
+    )
+}
 
-export default SearchBar;
+export default SearchBar
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,17 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import Layout from "./pages/Layout";
+import { createBrowserRouter } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Layout from './pages/Layout'
+import ErrorPage from './pages/ErrorPage'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    // errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <HomePage /> },
-      // { path: 'games/:slug', element: <GameDetailPage /> }
-    ]
-  }
-]);
+    {
+        path: '/',
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children: [
+            { index: true, element: <HomePage /> },
+            // { path: 'games/:slug', element: <GameDetailPage /> }
+        ],
+    },
+])
 
-export default router;
+export default router
