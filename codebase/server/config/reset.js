c3fb757600c69bf01cc324179a841318ffba5a55
Johnny <jac188@duke.edu>
2023-11-07 02:53:16 -0500

    set up frontend enviornment and some backend

---
+++ codebase/server/config/reset.js
@@ -0,0 +1,109 @@
+import { pool } from './database.js'
+import './dotenv.js'
+
+const createItemsTable = async () => {
+    const createItemsTableQuery = `
+    CREATE TABLE IF NOT EXISTS items (
+        id serial PRIMARY KEY,
+        title varchar(100) NOT NULL,
+        description varchar(500),
+        img_url text NOT NULL,
+        quantity integer,
+        price money NOT NULL,
+        color varchar(100),
+        type varchar(100) NOT NULL,
+        metal varchar(100),
+    );
+    `
+    try {
+        const res = await pool.query(createItemsTableQuery)
+        console.log('🎉 items created successfully')
+    } catch (err) {
+        console.error('⚠️ error creating items table', err)
+    }
+}
+
+const createUsersTable = async () => {
+    const createUsersTableQuery = `
+    CREATE TABLE IF NOT EXISTS users (
+        id serial PRIMARY KEY,
+        email varchar(100) NOT NULL,
+        password varchar(100) NOT NULL,
+        first_name varchar(100),
+        last_name varchar(100),
+        address varchar(100),
+        city varchar(100),
+        state varchar(100),
+        zip varchar(100),
+        phone varchar(100),
+    );
+    `
+    try {
+        const res = await pool.query(createUsersTableQuery)
+        console.log('🎉 users created successfully')
+    } catch (err) {
+        console.error('⚠️ error creating users table', err)
+    }
+}
+
+const createUsersSavedItemsTable = async () => {
+    const createUsersSavedItemsTableQuery = `
+    CREATE TABLE IF NOT EXISTS users_saved_items (
+        item_id int NOT NULL,
+        user_id int NOT NULL,
+        PRIMARY KEY (item_id, user_id),
+        FOREIGN KEY (item_id) REFERENCES items(id) ON UPDATE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE
+    );
+    `
+    try {
+        const res = await pool.query(createUsersSavedItemsTableQuery)
+        console.log('🎉 users_saved_items created successfully')
+    } catch (err) {
+        console.error('⚠️ error creating users_saved_items table', err)
+    }
+}
+
+const createUserCartItemsTable = async () => {
+    const createUserCartItemsTableQuery = `
+    CREATE TABLE IF NOT EXISTS users_cart_items (
+        item_id int NOT NULL,
+        user_id int NOT NULL,
+        PRIMARY KEY (item_id, user_id),
+        FOREIGN KEY (item_id) REFERENCES items(id) ON UPDATE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE
+    );
+    `
+    try {
+        const res = await pool.query(createUserCartItemsTableQuery)
+        console.log('🎉 users_cart_items created successfully')
+    } catch (err) {
+        console.error('⚠️ error creating users_cart_items table', err)
+    }
+}
+
+const createUserOrderTable = async () => {
+    const createUserOrderTableQuery = `
+    CREATE TABLE IF NOT EXISTS users_orders (
+        item_id int NOT NULL,
+        user_id int NOT NULL,
+        order_date date NOT NULL,
+        quantity int NOT NULL,
+        PRIMARY KEY (item_id, user_id),
+        FOREIGN KEY (item_id) REFERENCES items(id) ON UPDATE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE
+    );
+    `
+    try {
+        const res = await pool.query(createUserOrderTableQuery)
+        console.log('🎉 users_orders created successfully')
+    } catch (err) {
+        console.error('⚠️ error creating users_orders table', err)
+    }
+}
+
+createItemsTable()
+createUsersTable()
+createUsersSavedItemsTable()
+createUserCartItemsTable()
+createUserOrderTable()
---
+++ codebase/server/routes.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+// import tripRoutes from './routes/trips.js'
+// import activityRoutes from './routes/activities.js'
+// import destinationRoutes from './routes/destinations.js'
+// import tripDestinationRoutes from './routes/trips_destinations.js'
+
+// create express app
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
+})
+
+// app.use('/api/trips', tripRoutes)
+// app.use('/api/activities', activityRoutes)
+// app.use('/api/destinations', destinationRoutes)
+// app.use('/api/trips-destinations', tripDestinationRoutes)
+
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+})
