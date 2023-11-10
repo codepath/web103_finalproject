8cfe8adff25074a83aa2b9bb5c19c4d3ff28666a
Manya Bondada <manya.bondada@gmail.com>
2023-11-10 00:08:06 -0600

    database set up + data seeding

---
+++ server/data/boardMembersData.js
@@ -0,0 +1,20 @@
+const boardMembersData = [
+    {
+      "board_member_id": "1",
+      "board_id": "1"
+    },
+    {
+      "board_member_id": "2",
+      "board_id": "1"
+    }
+  ]
+  
+  export default boardMembersData
+
+  /*
+  Template
+    {
+      "board_member_id": "",
+      "board_id": ""
+    }
+  */
---
+++ server/data/usersData.js
@@ -0,0 +1,30 @@
+const usersData = [
+    {
+      "user_id": "1",
+      "name": "Manya",
+      "email": "manyaemail@something.com",
+      "password": "manyapwd123",
+      "phone_number": 123456789
+    },
+    {
+      "user_id": "2",
+      "name": "Arpit",
+      "email": "arpitemail@something.com",
+      "password": "arpitpwd123",
+      "phone_number": 987654321
+    }
+  ]
+  
+  export default usersData
+
+
+  /*
+  Template
+      {
+      "user_id": "",
+      "name": "",
+      "email": "",
+      "password": "",
+      "phone_number": ""
+    }
+  */
