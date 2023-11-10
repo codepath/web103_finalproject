79ecaad7264bba3afc634d35fe21191382c3b7f5
felixng <ngthinh302@gmail.com>
2023-11-10 18:59:25 -0500

    fix: Respond when user is not authenticated

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.get("/login/success", (req, res) => {
   if (req.user) {
     res.status(200).json({ success: true, user: req.user });
+  } else {
+    res.status(401).json({ success: false, message: "failure" });
   }
 });
 
