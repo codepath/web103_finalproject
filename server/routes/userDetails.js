18b3f0509cf2fdd743e528000e61d3baa65dcb75
singharpt <104125829+singharpt@users.noreply.github.com>
2023-11-10 20:51:58 -0600

    made changes in the server file

--- server/routes/userDetails.js
+++ server/routes/userDetails.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import UserDetailsController from '../controllers/userDetails.js'
+import express from "express";
+import UserDetailsController from "../controllers/userDetails.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/user/:user_id', UserDetailsController.getUserByUserId)
-router.get('/user/boards/:user_id', UserDetailsController.getBoardsByUserId)
-router.get('/user/tasks/:user_id', UserDetailsController.getTasksByUserId)
+router.get("/user/:user_id", UserDetailsController.getUserByUserId);
+router.get("/user/boards/:user_id", UserDetailsController.getBoardsByUserId);
+router.get("/user/tasks/:user_id", UserDetailsController.getTasksByUserId);
 
-export default router
+export default router;
