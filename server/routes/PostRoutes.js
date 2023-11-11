7ad67e0d51cb9410647027dc9790e87d34d816bb
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-11 14:51:20 -0500

    Update PostRoutes.js with patch and delete

--- server/routes/PostRoutes.js
+++ server/routes/PostRoutes.js
@@ -1,14 +1,12 @@
 import express from 'express';
-import { getAllPosts, createPost, getPostById } from '../controllers/PostController.js';
+import { getAllPosts, createPost, getPostById, updatePost, deletePost } from '../controllers/PostController.js';
 
 const router = express.Router();
 
-router.get('/', getAllPosts); //read
-router.post('/', createPost); //create
-router.get('/:id', getPostById); // New route for getting a single post
-
-// Uncomment and implement these in PostController.js if needed
-// router.patch('/:id', updatePost);
-// router.delete('/:id', deletePost);
+router.get('/', getAllPosts);
+router.post('/', createPost);
+router.get('/:id', getPostById);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
