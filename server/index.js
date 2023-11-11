9b71ff5c39929a8783ecc5ea1bd96e889fc9f883
felixng <ngthinh302@gmail.com>
2023-11-10 22:42:56 -0500

    refactor: Extract configs from server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ import setup from "./src/utils/setup.js";
 import passport from "passport";
 import session from "express-session";
 import { GitHub } from "./src/configs/auth.config.js";
+import { sessionConfig } from "./src/configs/session.config.js";
+import { corsConfig } from "./src/configs/cors.config.js";
 import { checkAuthentication } from "./src/middlewares/checkAuthentication.js";
 
 import authRouter from "./src/routes/auth.js";
@@ -17,21 +19,14 @@ setup();
 const app = express();
 
 app.use(morgan(process.env.NODE_ENV === "production" ? "common" : "dev"));
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+app.use(session(sessionConfig));
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1); // trust first proxy required for https
+}
+app.use(cors(corsConfig));
+
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  })
-);
+app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
