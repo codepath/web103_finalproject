1cdc43e3504ea70b821e6fe49d6cbd16f8ba4131
Myesha <maisha.mahjabin27@gmail.com>
2023-11-11 14:21:35 -0500

    Created AboutUs page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+// App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from "./components/Navigation";
@@ -6,6 +7,7 @@ import DiscussionBoard from './pages/DiscussionBoard';
 import ResourceList from './pages/ResourceList';
 import CreatePost from './pages/CreatePost';
 import CreateResource from './pages/CreateResource';
+import AboutUs from './pages/AboutUs'; // Import the new AboutUs component
 
 import './App.css';
 import SignUp from './pages/SignUp';
@@ -13,22 +15,20 @@ import Login from './pages/Login';
 
 const App = () => {
   return (
-      <div>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<DiscussionBoard />} />
-          <Route path="/resources" element={<ResourceList />} />
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/add-resource" element={<CreateResource />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-      
-          
-          
-        </Routes>
-      </div>
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<DiscussionBoard />} />
+        <Route path="/resources" element={<ResourceList />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/add-resource" element={<CreateResource />} />
+        <Route path="/about" element={<AboutUs />} /> {/* Add this line for the About Us page */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
