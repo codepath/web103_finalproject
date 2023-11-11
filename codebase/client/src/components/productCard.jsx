12150a07f9d06956eb2a52f036845760b1452b20
Johnny <jac188@duke.edu>
2023-11-11 00:44:17 -0500

    added product card

--- codebase/client/src/components/productCard.jsx
+++ codebase/client/src/components/productCard.jsx
@@ -1,35 +1,36 @@
 import React from "react";
-import { MDBRipple } from "mdb-react-ui-kit";
+import "../styles/productCard.css";
 
 function ProductCard({ imageUrl, price }) {
   return (
-    <MDBRipple
-      rippleColor="dark"
-      rippleTag="div"
-      className="bg-image rounded hover-zoom shadow-1-strong"
-    >
-      <img src={imageUrl} fluid="true" className="w-100" />
-      <a href="#!">
-        <div
-          className="mask"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
-        >
-          <div className="d-flex justify-content-start align-items-start h-100">
-            <h5>
-              <span className="badge bg-light pt-2 ms-3 mt-3 text-dark">
-                {price}
-              </span>
-            </h5>
+    <>
+          <div class="product">
+          <div class="imgbox"> 
+          <img src="https://i.imgur.com/iDwDQ4o.png"/> 
           </div>
-        </div>
-        <div className="hover-overlay">
-          <div
-            className="mask"
-            style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-          ></div>
-        </div>
-      </a>
-    </MDBRipple>
+          <div class="specifies">
+              <h2>Analog Watch 
+              <br/> 
+              <span>Rolex</span>
+              </h2>
+              <div class="price">$1500</div> <label>Size</label>
+              <ul>
+                  <li>10MM</li>
+                  <li>20MM</li>
+                  <li>30MM</li>
+                  <li>40MM</li>
+                  <li>50MM</li>
+              </ul> <label>Colors</label>
+              <ul class="colors">
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+              </ul> <button class="btn btn-primary">Buy Now</button>
+          </div>
+      </div>
+    </>
   );
 }
 
