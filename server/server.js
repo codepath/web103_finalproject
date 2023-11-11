335ce4ad9fe1f6de4835a0187aa866071d513588
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-11-10 22:48:44 -0500

    passport and sessions

--- server/server.js
+++ server/server.js
@@ -1,60 +1,67 @@
 import express from "express";
 import cors from "cors";
 import listingsRoutes from "./routes/listings.js";
-// import tripRoutes from "./routes/trips.js";
-// import activityRoutes from "./routes/activities.js";
-// import destinationRoutes from "./routes/destinations.js";
-// import tripDestinationRoutes from "./routes/trips_destinations.js";
+import propertiesRoutes from "./routes/properties.js";
 import authRoutes from "./routes/auth.js";
 import passport from "passport";
-// import session from "express-session";
+import session from "express-session";
 // import { GitHub } from "./config/auth.js";
 import "./config/dotenv.js";
+import cookieParser from "cookie-parser";
+import "./strategies/local.js";
 
 const app = express();
-
-// app.use(
-//   session({
-//     secret: process.env.CLIENT_SECRET, //need random string in .env
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
-
+const PORT = process.env.PORT || 3001;
+const memoryStore = new session.MemoryStore();
 app.use(express.json());
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     credentials: true,
-//   })
-// );
+app.use(cookieParser());
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(
+  session({
+    secret: process.env.CLIENT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: memoryStore,
+  })
+);
+// app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 // passport.use(GitHub);
 
 //to implement own strategy, can these functions in a custom way
-// passport.serializeUser((user, done) => {
-//   done(null, user);
-// });
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-// passport.deserializeUser((user, done) => {
-//   done(null, user);
-// });
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 app.get("/", (req, res) => {
   res.redirect("http://localhost:3000");
 });
-app.use("/auth", authRoutes);
+
+app.use((req, res, next) => {
+  console.log("Incoming Request Body:", req.body);
+  next();
+});
 // app.use("/api/trips", tripRoutes);
 // app.use("/api/activities", activityRoutes);
 // app.use("/api/destinations", destinationRoutes);
 // app.use("/api/trips-destinations", tripDestinationRoutes);
 
 app.use("/api", listingsRoutes);
-const PORT = process.env.PORT || 3001;
+app.use("/api/auth", authRoutes);
+app.use("/api/properties", propertiesRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
