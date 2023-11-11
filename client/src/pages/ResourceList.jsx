9abf94f420535c9b03e194f0b84de57ec35651bc
Myesha <maisha.mahjabin27@gmail.com>
2023-11-10 23:03:54 -0500

    Created ResourceList page

--- client/src/pages/ResourceList.jsx
+++ client/src/pages/ResourceList.jsx
@@ -1,29 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { getAllResources } from '../services/resourceService';
+// ResourceList.jsx
+import React from 'react';
+import { mockData } from '../../../server/data/mockData'; 
 import ResourceItem from '../components/ResourceItem';
+import './ResourceList.css';
+import Navbar from '../components/Navbar';
 
 const ResourceList = () => {
-  const [resources, setResources] = useState([]);
-
-  useEffect(() => {
-    const fetchResources = async () => {
-      try {
-        const data = await getAllResources();
-        setResources(data);
-      } catch (error) {
-        console.error('Failed to fetch resources:', error);
-      }
-    };
-
-    fetchResources();
-  }, []);
+  
+  const groupedResources = mockData.resources.reduce((acc, resource) => {
+    const category = resource.category || 'Other';
+    acc[category] = [...(acc[category] || []), resource];
+    return acc;
+  }, {});
 
   return (
     <div>
+          <Navbar />
+    <div className="resource-list-container">
       <h2>Resource List</h2>
-      {resources.map(resource => (
-        <ResourceItem key={resource.id} resource={resource} />
+
+    
+      {Object.entries(groupedResources).map(([category, resourcesInCategory]) => (
+        
+        
+          <div key={category} className="resource-section">
+          <h3>{category}</h3>
+          {resourcesInCategory.map(resource => (
+            <ResourceItem key={resource.id} resource={resource} />
+          ))}
+        </div>
+        
       ))}
+    </div>
     </div>
   );
 };
