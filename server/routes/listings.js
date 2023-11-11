39e1613f4fc74bcefdf68b26ab32d47e4bfea89f
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-11-10 22:48:27 -0500

    routes for listings

--- server/routes/listings.js
+++ server/routes/listings.js
@@ -5,5 +5,18 @@ import ListingsController from "../controllers/listings.js";
 const router = express.Router();
 
 router.get("/", ListingsController.getAllListings);
+router.get("/:id", ListingsController.getListingById);
+
+router.get("/listings", (request, response) => {});
+router.post("/listings/:propertyId", ListingsController.postNewListing);
+router.get("/listings/:userId/all", ListingsController.getUserListings);
+router.post(
+  "/listings/images/:userId",
+  ListingsController.postNewListingImages
+);
+router.post(
+  "/listings/availability/:listingId",
+  ListingsController.postNewListingAvailability
+);
 
 export default router;
