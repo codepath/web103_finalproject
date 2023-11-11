85b272f029ab05a5a4daa1d499cc4ad7fc0c8696
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-11 16:28:17 -0500

    edit and delete function

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,14 +1,25 @@
-// Post.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Post = ({ post, onDelete, onEdit }) => {
+  const handleDelete = () => {
+    if (typeof onDelete === 'function') {
+      onDelete(post.id);
+    }
+  };
+
+  const handleEdit = () => {
+    if (typeof onEdit === 'function') {
+      onEdit(post.id);
+    }
+  };
+
   return (
     <div className="post">
       <h3><Link to={`/posts/${post.id}`}>{post.title}</Link></h3>
       <p>{post.content}</p>
-      <button onClick={() => onEdit(post.id)}>Edit</button>
-      <button onClick={() => onDelete(post.id)}>Delete</button>
+      <button onClick={handleEdit}>Edit</button>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 };
