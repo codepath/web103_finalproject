270dc5ed6949ae4d1e0e465ca33065fba5f3f80f
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-11 13:42:37 -0500

    Update Routes in Express for getting a single post

--- server/routes/PostRoutes.js
+++ server/routes/PostRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getAllPosts, createPost } from '../controllers/PostController.js';
+import { getAllPosts, createPost, getPostById } from '../controllers/PostController.js';
 
 const router = express.Router();
 
 router.get('/', getAllPosts); //read
 router.post('/', createPost); //create
+router.get('/:id', getPostById); // New route for getting a single post
+
 // Uncomment and implement these in PostController.js if needed
 // router.patch('/:id', updatePost);
 // router.delete('/:id', deletePost);
