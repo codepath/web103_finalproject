ecf2fa9242d239a69f5ac9dd686b9a6a39634c19
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-11-13 16:56:04 -0500

    add propertiesReducer to root

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import propertiesReducer from "./slices/propertiesSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  properties: propertiesReducer,
 });
 
 export default rootReducer;
