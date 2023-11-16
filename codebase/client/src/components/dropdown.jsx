464566445899dd9b95b99e92abd1fb84ebb9cadb
Johnny <jac188@duke.edu>
2023-11-16 02:44:37 -0500

    added filtering

--- codebase/client/src/components/dropdown.jsx
+++ codebase/client/src/components/dropdown.jsx
@@ -1,43 +1,42 @@
-// components/Dropdown.jsx
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/dropdown.css';
 
-const Dropdown = ({ title, options, id }) => {
-  const [isChecked, setIsChecked] = useState(false);
+const Dropdown = ({ title, options, setParams, params, filter}) => {
+  const [selectedOption, setSelectedOption] = useState('');
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    const updatedFilters = {
+      ...params,
+      [filter]: option,
+    };
+    setParams(updatedFilters);
+    console.log(updatedFilters);
   };
 
   return (
     <>
       <div className="dropdown">
-        <input
-          type="checkbox"
-          id={id}
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-        />
-        <label htmlFor={id} className="dropdown-btn">
+        <label className="dropdown-btn">
           <span>{title}</span>
           <span className="arrow"></span>
         </label>
 
-        <ul className={`dropdown-content ${isChecked ? 'visible' : ''}`} role="menu">
+        <select
+          className="dropdown-content"
+          value={selectedOption}
+          onChange={(e) => handleOptionSelect(e.target.value)}>
+          <option value="">Select an option</option>
           {options.map((option, index) => (
-            <li key={index}><a href="#">{option}</a></li>
+            <option key={index} value={option}>
+              {option}
+            </option>
           ))}
-        </ul>
+        </select>
       </div>
     </>
   );
-};
-
-Dropdown.propTypes = {
-  title: PropTypes.string.isRequired,
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  id: PropTypes.string.isRequired,
 };
 
 export default Dropdown;
--- codebase/client/src/pages/catalog.jsx
+++ codebase/client/src/pages/catalog.jsx
@@ -6,24 +6,23 @@ import Dropdown from "../components/dropdown";
 import "../styles/catalog.css";
 
 const Catalog = () => {
-  const dropdownOptions = [
-    ['bracelets', 'earrings', 'necklaces & pendants', 'rings', 'other accessories'],
-    ['bracelets', 'earrings', 'necklaces & pendants', 'rings', 'other accessories']
-  ];
+  const [options, setOptions] = useState({
+  type: ['bracelets', 'earrings', 'necklaces & pendants', 'rings', 'other accessories'], 
+  color: ['silver', 'gold', 'rose gold', 'multi-color'],
+  metal: ['silver', 'gold', 'rose gold'],
+  minPrice: [10, 20, 50, 100, 200],
+  maxPrice: [20, 50, 100, 200, 500]
+  });
+
+  const [params, setParams] = useState({minPrice: undefined, maxPrice: undefined, color: '', type: '', metal: ''});
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     const fetchItems = async () => {
-    const queryParams = {
-      minPrice: 10,
-      maxPrice: 100,
-      color: 'silver',
-      type: 'bracelets',
-      metal: 'silver',
-    };
     
-    const queryString = Object.keys(queryParams)
-      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
+      const queryString = Object.entries(params)
+      .filter(([_, value]) => value !== undefined && value !== '')
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
       .join('&');
     
     try {
@@ -41,16 +40,18 @@ const Catalog = () => {
     }    
   }
   fetchItems();
-  }, []);
+  }, [params]);
 
 
   return (
     <>
       <h1 className="pageTitle">Catalog</h1>
       <div className="dropdowns">
-        {dropdownOptions.map((options, index) => (
-          <Dropdown key={index} title="Category" options={options} id={`dropdown-${index}`} />
-        ))}
+          <Dropdown title="Category" options={options.type} params={params} setParams={setParams} filter="type"/>
+          <Dropdown title="Color" options={options.color} params={params} setParams={setParams} filter="color"/>
+          <Dropdown title="Metal" options={options.metal} params={params} setParams={setParams} filter="metal"/>
+          <Dropdown title="Min Price" options={options.minPrice} params={params} setParams={setParams} filter="minPrice"/>
+          <Dropdown title="Max Price" options={options.maxPrice} params={params} setParams={setParams} filter="maxPrice"/>
       </div>
       <section className="cards">
         { items.length === 0 ? <p>No items found.</p> : items.map((item) => (
