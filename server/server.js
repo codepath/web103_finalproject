b0d5013330bf01940977a00a479b26999b657b2d
XujuanChen <xchen022@citymail.cuny.edu>
2023-11-16 18:49:01 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,22 +6,57 @@ import readersRouter from './routes/readers.js'
 import reviewsRouter from './routes/reviews.js'
 import readersbooksRouter from './routes/readersbooks.js'
 import BooksReviewsRouter from './routes/books_reviews.js'
+import userBookRoutes from './routes/users_books.js'
+
+import passport from 'passport'
+import session from 'express-session'
+import { GitHub } from './config/auth.js'
+import authRoutes from './routes/auth.js'
+
 
 dotenv.config()
 const app = express()
-app.use(express.json())
-app.use(cors())
 
+app.use(session({
+    secret: 'codepath',
+    resave: false,
+    saveUninitialized: true
+}))
+
+const CLIENT_URL = 'http://localhost:5173'
+
+app.use(express.json())
+app.use(cors({
+    origin: CLIENT_URL,
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    credentials: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(GitHub)
+
+passport.serializeUser((user, done) => {
+done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+done(null, user)
+})
 
 app.get('/', (req, res) => {
-    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;"> 📚 ReadRave API</h1>')
+    res.redirect(CLIENT_URL)
 })
+
+// authentication routes
+app.use('/auth', authRoutes)
 
 app.use('/api/books', booksRouter)
 app.use('/api/readers', readersRouter)
 app.use('/api/reviews', reviewsRouter)
 app.use('/api/readersbooks', readersbooksRouter)
 app.use('/api/booksreviews', BooksReviewsRouter)
+app.use('/api/users-books', userBookRoutes)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
