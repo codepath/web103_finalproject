70bd6bb5a8dc155f3edca12744565e6b657e8ba4
XujuanChen <xchen022@citymail.cuny.edu>
2023-11-17 12:24:56 -0500

    npm start

--- server/config/auth.js
+++ server/config/auth.js
@@ -4,7 +4,7 @@ import GitHubStrategy from 'passport-github2'
 const options = {
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  //callbackURL: 'http://localhost:3001/auth/github/callback'
+  callbackURL: 'http://localhost:3001/auth/github/callback'
 }
 
 const verify = async (accessToken, refreshToken, profile, callback) => {
