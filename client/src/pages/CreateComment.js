96d430944fa706faba3609a7021e1410e120ff71
Muhammad Faizan Abbasi <faizanx168@gmail.com>
2023-11-17 17:28:05 -0500

    final

--- client/src/pages/CreateComment.js
+++ client/src/pages/CreateComment.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import { TextField, Button, Typography } from "@material-ui/core";
 import { useParams } from "react-router-dom";
 
 const CreateReview = () => {
   const { id } = useParams();
   const [reviewData, setReviewData] = useState({
-    product_id: id, // Assign the product ID from the URL
-    user_id: 1, // Assuming you have a logged-in user with an ID
+    product_id: id,
+    user_id: 1,
     rating: "",
     review_text: "",
-    review_date: new Date().toISOString().split("T")[0], // Get today's date in YYYY-MM-DD format
+    review_date: new Date().toISOString().split("T")[0],
   });
 
   const handleChange = (e) => {
@@ -30,10 +29,8 @@ const CreateReview = () => {
 
       if (response.ok) {
         console.log("Review created successfully!");
-        // Optionally, handle success state or redirect the user
       } else {
         console.error("Failed to create review");
-        // Optionally, handle the failure case
       }
     } catch (error) {
       console.error("Error creating review:", error);
@@ -42,27 +39,29 @@ const CreateReview = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Typography variant="h5">Leave a Review</Typography>
-      <TextField
-        name="rating"
-        label="Rating (1-5)"
-        type="number"
-        InputProps={{ inputProps: { min: 1, max: 5 } }}
-        value={reviewData.rating}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        name="review_text"
-        label="Review Text"
-        multiline
-        value={reviewData.review_text}
-        onChange={handleChange}
-        required
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Submit Review
-      </Button>
+      <h2>Leave a Review</h2>
+      <label>
+        Rating (1-5)
+        <input
+          name="rating"
+          type="number"
+          min="1"
+          max="5"
+          value={reviewData.rating}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Review Text
+        <textarea
+          name="review_text"
+          value={reviewData.review_text}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <button type="submit">Submit Review</button>
     </form>
   );
 };
