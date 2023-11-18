f567aa836f8f86e6acc41ac151f5cc44f5fad87e
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-17 19:54:54 -0500

    Delete unused files and update build configuration(dist)

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/public',
+    outDir: 'dist',
     emptyOutDir: true
   },
   server: {
