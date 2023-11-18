6a4c5b92f67941b76cd54baebdb29d1be7e35bf7
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-11-17 19:24:11 -0500

    config fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,21 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()],
-    resolve: {
-        alias: {
-            '@': '/src',
-        },
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
-    build: {
-        outDir: '../server/public',
-        emptyOutDir: true
-      }
-    
-})
+  },
+});
