546c8f687edb6bdeecf78e1ce1ed64c18aaaa890
Hafeefa <hafeefa40@gmail.com>
2023-11-18 01:56:04 -0500

    added calendar components for check in and check out dates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,9 +5,13 @@ import stayvueLogo from "../assets/StayVue.png";
 import search from "../assets/search.png";
 import LoginModal from "./LoginModal";
 import DropdownMenu from "./DropdownMenu";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Navbar = ({ isModalOpen, setIsModalOpen }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
   const user = useSelector((state) => state.user?.loggedInUser);
 
   useEffect(() => {
@@ -31,6 +35,14 @@ const Navbar = ({ isModalOpen, setIsModalOpen }) => {
     console.log("MODAL open");
   };
 
+  const handleDateChange = (date, type) => {
+    if (type === "start") {
+      setStartDate(date);
+    } else {
+      setEndDate(date);
+    }
+  };
+
   return (
     <nav className="bg-white border-b-1 border-gray-300 w-full mt-4">
       <div className="flex justify-between items-center">
@@ -40,21 +52,37 @@ const Navbar = ({ isModalOpen, setIsModalOpen }) => {
           </Link>
         </div>
         <div className="search-bar rounded-3xl p-4 shadow-md flex items-center w-3/6 justify-evenly">
-          <div style={{ display: "contents" }}>
+          <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
             <button
-              className="border-none"
+              className="border-none w-8/12"
               onClick={() => handleButtonClick("Anywhere")}
             >
               Anywhere
             </button>
+            {/* Date picker for start date */}
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => handleDateChange(date, "start")}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              placeholderText="Check in"
+              className="w-9/12"
+            />
+            {/* Date picker for end date */}
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => handleDateChange(date, "end")}
+              className="w-9/12"
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              placeholderText="Check-out"
+            />
+          </div>
+          <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
             <button
-              className="border-none"
-              onClick={() => handleButtonClick("Any week")}
-            >
-              Any week
-            </button>
-            <button
-              className="border-none"
+              className="border-none w-9/12"
               onClick={() => handleButtonClick("Add guests")}
             >
               Add guests
@@ -63,7 +91,7 @@ const Navbar = ({ isModalOpen, setIsModalOpen }) => {
               className="border-none p-0"
               onClick={() => handleButtonClick("Search")}
             >
-              <img src={search} alt="search button" className="w-8 h-8" />
+              <img src={search} alt="search button" className="w-11 h-10" />
             </button>
           </div>
         </div>
