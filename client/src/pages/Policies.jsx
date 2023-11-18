6f38b5d04e43af389fcdbcd64eb47060db59273f
Hafeefa <hafeefa40@gmail.com>
2023-11-18 03:09:09 -0500

    added general policies for users/buyers

---
+++ client/src/pages/Policies.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+function Policies() {
+  return (
+    <div className="p-4 pt-14 ">
+      <h1 className="text-2xl font-bold mb-10 underline decoration-red-100">Policies</h1>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Booking and Reservation Policies:</strong> </p> Welcome to our unique spaces! Each property may have different booking requirements, ranging from a minimum of one night to longer stays. Explore the listings for details on age restrictions and the distinctive charm each owner brings to their space.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'> Check-in and Check-out: </strong> </p>  Check-in times vary depending on the property and its owner, starting at 3:00 PM. Check-out is generally by 11:00 AM, but please check your specific listing for details. Keys can be collected from the lockbox, providing a seamless arrival experience.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Cancellation Policy:</strong> </p> Cancellation policies are tailored to each owners preferences, ranging from fully refundable up to 7 days before the stay to more specific terms. Check your chosen propertys listing for cancellation details.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p><strong className='text-red-400'>House Rules:</strong></p> Embrace the diversity of our listings! While some spaces may allow smoking and pets, others maintain strict no-smoking policies or are pet-free. Be sure to review each propertys unique rules, such as noise levels and shared space usage.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Security Deposit:</strong> </p> The security deposit amount varies by property, with some owners requesting $100 and others more or less. Refer to your booking confirmation for details on the specific conditions for a refund.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'> Cleaning and Maintenance:</strong> </p> Cleaning fees vary based on the property, reflecting the owners commitment to maintaining a fresh and welcoming environment. Specific cleaning expectations are outlined in each listing, so guests are encouraged to review these details.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Guest Responsibilities:</strong> </p> Key handling guidelines are unique to each property. Some may require extra care due to high-security measures, so please check your specific listing for instructions. Familiarize yourself with community guidelines, as they may differ across properties.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Communication:</strong> </p> Owners may have different preferences for communication methods, ranging from Airbnb messaging to direct phone calls. Refer to your booking confirmation for the owners preferred contact details.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Local Regulations and Laws:</strong> </p> Discover the neighborhood responsibly by adhering to local laws and regulations. Waste disposal rules, local curfews, and other regulations may vary, so be sure to review property-specific guidelines.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Liability and Insurance:</strong> </p> While owners are committed to your safety, individual properties may have different insurance coverage. Guests are encouraged to consider travel insurance based on the specific propertys policies.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Guest Identification:</strong> </p> Some owners may request identification upon arrival for security purposes. Check your specific listing for details on guest identification requirements.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Accessibility:</strong> </p> Each owner strives to make their property accessible. If you have specific needs, please reach out to the individual owner to discuss accommodations tailored to your requirements.
+      </p>
+
+      <p className="text-gray-700 border-b-8 pb-4 mb-4">
+        <p> <strong className='text-red-400'>Feedback and Reviews:</strong> </p> Share your unique experiences! Each property owner values your feedback, and your reviews contribute to the diverse community of Airbnb. Be open to different hosting styles and provide constructive feedback based on your individual experience.
+      </p>
+    </div>
+  );
+}
+
+export default Policies;
