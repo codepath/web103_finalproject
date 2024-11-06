5874905237d5e4a26044d43ed3a365c6724e0aad
drakenevadie19 <drakennievadje19@gmail.com>
2024-11-06 01:03:05 -0600

    New Change: Addding navigation bar

--- front-end/src/components/nav-bar.jsx
+++ front-end/src/components/nav-bar.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import '../css/nav-bar.css'
+import { useNavigate } from "react-router-dom";
+
 const NavigationBar = () => {
+
+    const [loggedIn, setLoggedIn] = useState(true);
+    const navigate = useNavigate();
+
     return (
         <> 
-            <h1>This is navigation bar</h1>
+            <div className="nav-bar">
+                <h1 onClick={() => navigate("/")}>BOOKEZ</h1>
+
+                <div className="nav-bar-logged-in-status">
+                    {
+                        loggedIn 
+                        ?
+                            <div className="nav-bar-right-box">
+                                <button type="button" className="btn btn-light" onClick={() => navigate("/login")}>Log In</button>
+                                <button type="button" className="btn btn-info" onClick={() => navigate("/signup")}>Sign Up</button>
+                            </div>
+                        :
+                            <div className="nav-bar-right-box">
+                                <div className="nav-bar-profile-image-box">
+                                    <img src='../../data/profile_sample_image.jpg' className="nav-bar-profile-image-url" alt="Profile" />
+                                </div>
+                                <div className="nav-bar-username">
+                                    Drake Do=
+                                </div>
+                            </div>
+                    }
+                </div>
+            </div>
         </>
     )
 }
