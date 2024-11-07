217430001e17cf0032e7c2d1510f9ce7fe49815a
Chengtao <chengtaolinctl@gmail.com>
2024-11-06 20:55:43 -0500

    Created: Mock data

---
+++ BookEZ/server/mock_data/users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    id: 1,
+    username: 'ellen',
+    password: 'hashed_password_123',
+    email: 'ellen@example.com',
+    full_name: 'Ellen Joe',
+    phone_number: '123-456-7890',
+  },
+  {
+    id: 2,
+    username: 'janedoe',
+    password: 'hashed_password_456',
+    email: 'janedoe@example.com',
+    full_name: 'Jane Doe',
+    phone_number: '098-765-4321',
+  },
+]
+
+export default users
