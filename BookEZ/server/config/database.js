61a5415e1cca43400558455fc1771fe640b39af0
Chengtao <chengtaolinctl@gmail.com>
2024-11-06 23:24:43 -0500

    Completed: test db by using mock data

---
+++ BookEZ/server/config/database.js
@@ -0,0 +1,14 @@
+import pg from 'pg'
+
+const config = {
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}
+
+export const pool = new pg.Pool(config)
