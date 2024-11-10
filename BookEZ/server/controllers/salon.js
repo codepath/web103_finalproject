2ae80e2a578640b0ac70699bf82bc151950cb560
Parth Kansara <parth.kansara@stonybrook.edu>
2024-11-10 18:05:05 -0500

    Refined SELECT query

--- BookEZ/server/controllers/salon.js
+++ BookEZ/server/controllers/salon.js
@@ -10,10 +10,16 @@ const getAllSalons = async (req, res) => {
 }
 
 const getSalonById = async (req, res) => {
-    const salon_id = req.params.id;
+    const salon_id = req.params.salon_id;
     try {
-        const results = await pool.query('SELECT * FROM salons WHERE id = $1', [salon_id])
-        res.status(200).json(results);
+        const results = await pool.query('SELECT * FROM salons WHERE id=$1', [salon_id])
+        
+        if (results.rows.length === 0){
+            res.status(404).json({ error: "Salon not found" })
+
+        } else {
+            res.status(200).json(results.rows[0]);
+        }
     } catch (error) {
         res.status(409).json({ error: error.message })
     }
