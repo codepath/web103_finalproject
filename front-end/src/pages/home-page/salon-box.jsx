6642f8b49a128a1d46e3a6133091575b454d3ad4
drakenevadie19 <drakennievadje19@gmail.com>
2024-11-12 11:08:26 -0600

    New Change: Connected Salon list to Home Page

--- front-end/src/pages/home-page/salon-box.jsx
+++ front-end/src/pages/home-page/salon-box.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const SalonBox = ({ salon }) => {
+const SalonBox = ({ salonToDisplay }) => {
     // console.log(salon);
     const navigate = useNavigate();
+    const [salons, setSalons] = useState([]);
+    setSalons(salonToDisplay);
+
+    useEffect(() => {
+        if (salonToDisplay) {
+            setSalons(salonToDisplay);
+        }
+    }, [salonToDisplay]);
 
     return (
-        <>
-            <div className="salon-box">
-                <h3 className="sb-salon-name" onClick={() => navigate(`/salon/${salon.id}`)}>{salon.name}</h3>
-                <p>&#xf041; {salon.address}, {salon.city}, {salon.state} {salon.zip_code}</p>
-            </div>
-        </>
+        <div className="hps-list-of-salon">
+            {salons && salons.length > 0 ? (
+                salons.map((salon, index) => (
+                    <div className="salon-box" key={index} onClick={() => navigate(`/salon/${salon.id}`)}>
+                        <h3 className="sb-salon-name">{salon.name}</h3>
+                        <p>&#xf041; {salon.address}, {salon.city}, {salon.state} {salon.zip_code}</p>
+                    </div>
+                ))
+            ) : (
+                <p>No salons available to display</p>
+            )}
+
+        </div>
     )
 
 }
