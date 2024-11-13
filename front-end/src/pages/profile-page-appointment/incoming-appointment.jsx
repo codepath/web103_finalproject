1e8376b902706dc67ed819d0a7e10f77b2d11315
drakenevadie19 <drakennievadje19@gmail.com>
2024-11-12 23:08:45 -0600

    New Change: added basic pages for profile page

---
+++ front-end/src/pages/profile-page-appointment/incoming-appointment.jsx
@@ -0,0 +1,12 @@
+
+
+const IncomingAppointments = () => {
+
+    return (
+        <>
+            <h1>List of incoming appointment</h1>
+        </>
+    );
+}
+
+export default IncomingAppointments;
